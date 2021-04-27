eee2910853aa5cce5461d877d415aa39b92bad8a
hwu71 <hwu71@binghamton.edu>
2021-04-26 23:26:47 -0400

    Add test

--- examples/modbus_client.py
+++ examples/modbus_client.py
@@ -20,6 +20,7 @@ from pymodbus.transaction import DictTransactionManager
 logger = logging.getLogger("client")
 logger.setLevel('DEBUG')
 
+
 class ModbusUdpClientProtocol(ModbusClientMixin):
     def __init__(self, client):
         self.client = client
@@ -32,7 +33,7 @@ class ModbusUdpClientProtocol(ModbusClientMixin):
     # BaseModbusAsyncClientProtocol::execute
     async def execute(self, request=None):
         req = self._execute(request)
-        resp = await asyncio.shield(req)
+        resp = await asyncio.wait_for(req, timeout=2)
         return resp
 
     # BaseModbusAsyncClientProtocol::_execute
@@ -73,7 +74,6 @@ class ModbusUdpClientProtocol(ModbusClientMixin):
                 logger.debug("Unrequested message: " + str(reply))
 
 
-
 class ModbusClient(QuicConnectionProtocol):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -93,7 +93,7 @@ class ModbusClient(QuicConnectionProtocol):
         end_stream = False
         self._quic.send_stream_data(stream_id, data, end_stream)
 
-
+UNIT = 0x01
 async def start_async_test(client):
     # ----------------------------------------------------------------------- #
     # specify slave to query
@@ -102,7 +102,6 @@ async def start_async_test(client):
     # individual request. This can be done by specifying the `unit` parameter
     # which defaults to `0x00`
     # ----------------------------------------------------------------------- #
-    UNIT = 0x01
     logger.debug("Reading Coils")
     rr = await client.read_coils(1, 1, unit=0x01)
 
@@ -118,65 +117,65 @@ async def start_async_test(client):
     # Keep both of these cases in mind when testing as the following will
     # _only_ pass with the supplied asynchronous modbus server (script supplied).
     # ----------------------------------------------------------------------- #
-    # logger.debug("Write to a Coil and read back")
-    # rq = await client.write_coil(0, True, unit=UNIT)
-    # rr = await client.read_coils(0, 1, unit=UNIT)
-    # assert(rq.function_code < 0x80)     # test that we are not an error
-    # assert(rr.bits[0] == True)          # test the expected value
-    # #
-    # logger.debug("Write to multiple coils and read back- test 1")
-    # rq = await client.write_coils(1, [True]*8, unit=UNIT)
-    # assert(rq.function_code < 0x80)     # test that we are not an error
-    # rr = await client.read_coils(1, 21, unit=UNIT)
-    # assert(rr.function_code < 0x80)     # test that we are not an error
-    # resp = [True]*21
-    #
-    # # If the returned output quantity is not a multiple of eight,
-    # # the remaining bits in the final data byte will be padded with zeros
-    # # (toward the high order end of the byte).
-    #
-    # resp.extend([False]*3)
-    # assert(rr.bits == resp)         # test the expected value
-    #
-    # logger.debug("Write to multiple coils and read back - test 2")
-    # rq = await client.write_coils(1, [False]*8, unit=UNIT)
-    # rr = await client.read_coils(1, 8, unit=UNIT)
-    # assert(rq.function_code < 0x80)     # test that we are not an error
-    # assert(rr.bits == [False]*8)         # test the expected value
-    #
-    # logger.debug("Read discrete inputs")
-    # rr = await client.read_discrete_inputs(0, 8, unit=UNIT)
-    # assert(rq.function_code < 0x80)     # test that we are not an error
-    #
-    # logger.debug("Write to a holding register and read back")
-    # rq = await client.write_register(1, 10, unit=UNIT)
-    # rr = await client.read_holding_registers(1, 1, unit=UNIT)
-    # assert(rq.function_code < 0x80)     # test that we are not an error
-    # assert(rr.registers[0] == 10)       # test the expected value
-    #
-    # logger.debug("Write to multiple holding registers and read back")
-    # rq = await client.write_registers(1, [10]*8, unit=UNIT)
-    # rr = await client.read_holding_registers(1, 8, unit=UNIT)
-    # assert(rq.function_code < 0x80)     # test that we are not an error
-    # assert(rr.registers == [10]*8)      # test the expected value
-    #
-    # logger.debug("Read input registers")
-    # rr = await client.read_input_registers(1, 8, unit=UNIT)
-    # assert(rq.function_code < 0x80)     # test that we are not an error
-    #
-    # arguments = {
-    #     'read_address':    1,
-    #     'read_count':      8,
-    #     'write_address':   1,
-    #     'write_registers': [20]*8,
-    # }
-    # logger.debug("Read write registeres simulataneously")
-    # rq = await client.readwrite_registers(unit=UNIT, **arguments)
-    # rr = await client.read_holding_registers(1, 8, unit=UNIT)
-    # assert(rq.function_code < 0x80)     # test that we are not an error
-    # assert(rq.registers == [20]*8)      # test the expected value
-    # assert(rr.registers == [20]*8)      # test the expected value
-    # await asyncio.sleep(1)
+    logger.debug("Write to a Coil and read back")
+    rq = await client.write_coil(0, True, unit=UNIT)
+    rr = await client.read_coils(0, 1, unit=UNIT)
+    assert(rq.function_code < 0x80)     # test that we are not an error
+    assert(rr.bits[0] == True)          # test the expected value
+
+    logger.debug("Write to multiple coils and read back- test 1")
+    rq = await client.write_coils(1, [True]*8, unit=UNIT)
+    assert(rq.function_code < 0x80)     # test that we are not an error
+    rr = await client.read_coils(1, 21, unit=UNIT)
+    assert(rr.function_code < 0x80)     # test that we are not an error
+    resp = [True]*21
+
+    # If the returned output quantity is not a multiple of eight,
+    # the remaining bits in the final data byte will be padded with zeros
+    # (toward the high order end of the byte).
+
+    resp.extend([False]*3)
+    assert(rr.bits == resp)         # test the expected value
+
+    logger.debug("Write to multiple coils and read back - test 2")
+    rq = await client.write_coils(1, [False]*8, unit=UNIT)
+    rr = await client.read_coils(1, 8, unit=UNIT)
+    assert(rq.function_code < 0x80)     # test that we are not an error
+    assert(rr.bits == [False]*8)         # test the expected value
+
+    logger.debug("Read discrete inputs")
+    rr = await client.read_discrete_inputs(0, 8, unit=UNIT)
+    assert(rq.function_code < 0x80)     # test that we are not an error
+
+    logger.debug("Write to a holding register and read back")
+    rq = await client.write_register(1, 10, unit=UNIT)
+    rr = await client.read_holding_registers(1, 1, unit=UNIT)
+    assert(rq.function_code < 0x80)     # test that we are not an error
+    assert(rr.registers[0] == 10)       # test the expected value
+
+    logger.debug("Write to multiple holding registers and read back")
+    rq = await client.write_registers(1, [10]*8, unit=UNIT)
+    rr = await client.read_holding_registers(1, 8, unit=UNIT)
+    assert(rq.function_code < 0x80)     # test that we are not an error
+    assert(rr.registers == [10]*8)      # test the expected value
+
+    logger.debug("Read input registers")
+    rr = await client.read_input_registers(1, 8, unit=UNIT)
+    assert(rq.function_code < 0x80)     # test that we are not an error
+
+    arguments = {
+        'read_address':    1,
+        'read_count':      8,
+        'write_address':   1,
+        'write_registers': [20]*8,
+    }
+    logger.debug("Read write registeres simulataneously")
+    rq = await client.readwrite_registers(unit=UNIT, **arguments)
+    rr = await client.read_holding_registers(1, 8, unit=UNIT)
+    assert(rq.function_code < 0x80)     # test that we are not an error
+    assert(rq.registers == [20]*8)      # test the expected value
+    assert(rr.registers == [20]*8)      # test the expected value
+    await asyncio.sleep(1)
 
 def save_session_ticket(ticket):
     """
